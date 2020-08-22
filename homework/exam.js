604732a832cb2ad72dd9bf7d290df146c79fb754
bit716 <sch030518@gmail.com>
2020-08-22 19:52:01 +0900

    jQuery -> fetch API

--- homework/exam.js
+++ homework/exam.js
@@ -5,18 +5,7 @@ const appid = '가입하고 얻는 appid';
 // 좌표로부터 날씨 정보 가져옴
 function getWeatherFromRegion(region) {
     var result = "";
-    $.ajax({
-        url: `강의 밑 API 문서 참고하여 채워넣을 것`,
-        type: "GET",
-        async: false, // 동기로 바꿔주면서 리턴값 얻음
-        success: function (data) {
-            if (data) {
-                result = data;
-            } else {
-                alert("불러오기 실패");
-            }
-        }
-    });
+    // fetch API 사용하여
     return result;
 };
 
