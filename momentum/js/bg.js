a25f1328652b9c721879c85cbc8a1c737359c36f
Jun <sch030518@gmail.com>
2020-08-25 16:03:31 +0900

    add image on background

---
+++ momentum/js/bg.js
@@ -0,0 +1,32 @@
+const body = document.querySelector('body');
+
+const IMAGE_AMOUNT = 6;
+
+// function handleImgLoad()
+// {
+//     console.log('Image loaded!');
+// }
+
+function paintImage(imgNumber)
+{
+    const image = new Image();
+    image.src = `img/${imgNumber + 1}.jpg`;
+    body.appendChild(image);
+    image.classList.add('bgImg');
+    // body.prepend(image);
+    // image.addEventListener('loadend', handleImgLoad);
+}
+
+function getRandom()
+{
+    const number = Math.floor(Math.random() * IMAGE_AMOUNT);
+    return number;
+}
+
+function init()
+{
+    const randomNumber = getRandom();
+    paintImage(randomNumber);
+};
+
+init();
