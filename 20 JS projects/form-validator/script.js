9a84f6833de3ea690c2faa5532c0fae2655eac33
Jun <sch030518@gmail.com>
2020-09-03 23:54:26 +0900

    Check length, email and password match

--- 20 JS projects/form-validator/script.js
+++ 20 JS projects/form-validator/script.js
@@ -21,53 +21,76 @@ function showSuccess(input)
 }
 
 // Check email is valid
-function isValidEmail(email)
+function checkEmail(input)
 {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    if (re.test(input.value.trim()))
+    {
+        showSuccess(input);
+    }
+    else
+    {
+        showError(input, 'Email is not valid');
+    }
+}
+
+
+// Check required fields
+function checkRequired(inputs)
+{
+    inputs.forEach((input) => {
+        // To trim out any whitespace
+        if (input.value.trim() === '')
+        {
+            showError(input, `${getFieldName(input)} is required`);
+        }
+        else
+        {
+            showSuccess(input);
+        }
+    })
+}
+
+// Check input length
+function checkLength(input, min, max)
+{
+    if (input.value.length < min)
+    {
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+    }
+    else if (input.value.length > max)
+    {
+        showError(input, `${getFieldName(input)} must be less than ${max} characters`);
+    }
+    else
+    {
+        showSuccess(input);
+    }
+}
+
+// Check passwords match
+function checkPasswordsMatch(input1, input2)
+{
+    if (input1.value !== input2.value)
+    {
+        showError(input2, 'Passwords do not match');
+    }
+}
+
+// Get field name
+function getFieldName(input)
+{
+    const id = input.id.replace(/_/, ' ');
+    return id.charAt(0).toUpperCase() + id.slice(1);
 }
 
 // Event Listeners
 form.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    if (username.value === '')
-    {
-        showError(username, 'Hey, put the username in!');
-    }
-    else
-    {
-        showSuccess(username);
-    }
-
-    if (email.value === '')
-    {
-        showError(email, 'Hey, put the email in!');
-    }
-    else if (!isValidEmail(email.value))
-    {
-        showError(email, 'Hey, put the valid email in!');
-    }
-    else
-    {
-        showSuccess(email);
-    }
-
-    if (password.value === '')
-    {
-        showError(password, 'Hey, put the password in!');
-    }
-    else
-    {
-        showSuccess(password);
-    }
-
-    if (confirm_password.value === '')
-    {
-        showError(confirm_password, 'Hey, put the confirm password in!');
-    }
-    else
-    {
-        showSuccess(confirm_password);
-    }
+    checkRequired([username, email, password, confirm_password]);
+    checkLength(username, 3, 15);
+    checkLength(password, 6, 25);
+    checkEmail(email);
+    checkPasswordsMatch(password, confirm_password);
 })
