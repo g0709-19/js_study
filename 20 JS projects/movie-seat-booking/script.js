163aec610ee4cd209007781840aa88687974dfe1
Jun <sch030518@gmail.com>
2020-09-04 17:05:37 +0900

    Selecting movie and seats from UI

---
+++ 20 JS projects/movie-seat-booking/script.js
@@ -0,0 +1,35 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+
+// Instead of parseInt(String) We can use +
+let ticketPrice = +movieSelect.value;
+
+// Update total and count
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+    const selectedSeatsCount = selectedSeats.length;
+
+    count.innerText = selectedSeatsCount;
+    total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+// Movie select event
+movieSelect.addEventListener('change', event => {
+    ticketPrice = +event.target.value;
+    updateSelectedCount();
+});
+
+// Seat click event
+// 1. seats.forEach
+// 2. container.addEventListener
+container.addEventListener('click', event => {
+    if (event.target.classList.contains('seat') &&
+        !event.target.classList.contains('occupied')) {
+        event.target.classList.toggle('selected');
+    }
+
+    updateSelectedCount();
+});
