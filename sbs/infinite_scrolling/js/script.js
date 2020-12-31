a64ffd53af1414fa035bbd1ac9134d68039ade4d
Jun <sch030518@gmail.com>
2020-12-31 09:40:10 +0900

    클래스 방식으로 하니 오류남

--- sbs/infinite_scrolling/js/script.js
+++ sbs/infinite_scrolling/js/script.js
@@ -1,8 +1,28 @@
 
-const POST_AMOUNT = 100;
-const posts = document.getElementById('posts-js');
+let Scroll = function() {
+    this.POST_AMOUNT = 100;
+    this.posts = document.getElementById('posts-js');
+    this.LIMIT = 5;
+    this.page = 1;
+    this.filter = document.getElementById('filter-js');
+}
 
-function createPost(index) {
+Scroll.prototype.getLorem = function (index) {
+    const url = `https://jsonplaceholder.typicode.com/posts?_limit=${this.LIMIT}&_page=${this.page}`;
+    return fetch(url).then(response => response.json())
+    .then(json => {
+        index %= this.LIMIT;
+        const title = json[index].title;
+        const body = json[index].body.replaceAll('\n', ' ');
+        let result = {
+            title,
+            body
+        }
+        return result;
+    });
+}
+
+Scroll.prototype.createPost = function (index) {
     const post = document.createElement('div');
     post.classList.add('post');
 
@@ -17,10 +37,14 @@ function createPost(index) {
     info_element.classList.add('info');
 
     const h2 = document.createElement('h2');
-    h2.innerText = '뭔가 로렘 입숨 제목';
-
     const p = document.createElement('p');
-    p.innerText = '뭔가 로렘 입숨 내용';
+
+    const lorem = this.getLorem(index-1);
+
+    lorem.then(result => {
+        h2.innerText = result.title;
+        p.innerText = result.body;
+    });
 
     info_element.appendChild(h2);
     info_element.appendChild(p);
@@ -33,15 +57,61 @@ function createPost(index) {
     return post;
 }
 
-function initPosts() {
-    for (let i = 1; i <= POST_AMOUNT; ++i) {
-        const post = createPost(i);
-        posts.appendChild(post);
+Scroll.prototype.initPosts = function () {
+    for (let i = 1; i <= this.POST_AMOUNT; i += this.LIMIT) {
+        for (let j = i; j < (i + this.LIMIT); ++j) {
+            const post = this.createPost(j);
+            this.posts.appendChild(post);
+        }
+        ++this.page;
     }
 }
 
+Scroll.prototype.handleInput = function () {
+    //console.dir(this.posts);
+    const filter = document.getElementById('filter-js');
+    const inputed = filter.value;
+
+    const posts = document.getElementById('posts-js');
+    const childs = [...posts.querySelectorAll('.post')];
+
+    // 입력된 값에 부합하는 노드들만 걸러냄
+    const filtered = childs.filter(child => {
+        const box = child.querySelector('.box');
+        const info = box.querySelector('.info');
+        const title = info.querySelector('h2');
+        const body = info.querySelector('p');
+        const title_text = title.innerText;
+        const body_text = body.innerText;
+        
+        return title_text.indexOf(inputed) !== -1 ||
+            body_text.indexOf(inputed) !== -1;
+    });
+
+    {
+        let i = 0;
+        
+        const container = posts.parentNode;
+        this.posts = document.createElement('div');
+        this.posts.id = 'posts-js';
+        this.posts.classList.add('posts');
+        filtered.forEach(node => {
+            const post = this.createPost(i);
+            this.posts.appendChild(post);
+            ++i;
+        });
+        
+    }
+
+    console.log(filtered);
+    
+}
+
+
 function init() {
-    initPosts();
+    let scroll = new Scroll();
+    scroll.initPosts();
+    scroll.filter.addEventListener('input', scroll.handleInput);
 }
 
 init();
